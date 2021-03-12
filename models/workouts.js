d68d5f711f8ed26da1f55affdbc0abc0598958e9
yvettehenderson <yvette.henderson@gmail.com>
2021-03-12 10:40:27 -0800

    work out and index

---
+++ models/workouts.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Please enter in an  exercise type',
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Please enter in an  exercise name',
+      },
+      duration: {
+        type: Number,
+        required: 'Please enter in the amount of time of the workout in minutes',
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workouts = mongoose.model('Workouts', workoutSchema);
+
+module.exports = Workouts;
