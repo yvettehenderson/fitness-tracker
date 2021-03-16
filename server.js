67ce9f45fbb36e2fd5ff1245845c93af055963ca
yvettehenderson <yvette.henderson@gmail.com>
2021-03-16 15:42:41 -0700

    route update

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT ||3000;
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan("tiny"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
