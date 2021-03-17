8554361a9a212d4b6883134fc8ef397aca8ddec9
yvettehenderson <yvette.henderson@gmail.com>
2021-03-17 10:25:33 -0700

    updated db connection

--- server.js
+++ server.js
@@ -13,10 +13,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Fitness Tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
