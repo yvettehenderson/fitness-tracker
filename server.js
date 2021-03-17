31eef030d483790be3b07865eea9c96b382065cd
yvettehenderson <yvette.henderson@gmail.com>
2021-03-17 12:20:01 -0700

    dev

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ||3000;
 
 const app = express();
 
-app.use(morgan("tiny"));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
